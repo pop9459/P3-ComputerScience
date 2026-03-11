e32913f3e6e9c221519a3d8b6424545f1ed9c45c
Peter Kapsiar <kapsiar888n@gmail.com>
2026-03-11 13:52:27 +0100

    made unit tests for refueling

--- Refueling/fuel.py
+++ Refueling/fuel.py
@@ -2,7 +2,7 @@ def main():
     while True:
         try:
             input_fraction = input("Fraction: ")
-            fuel_gauge = gauge(input_fraction)
+            fuel_gauge = gauge(convert(input_fraction))
         except (ValueError, ZeroDivisionError):
             pass
         else:
@@ -20,24 +20,22 @@ def convert(fraction):
     Y = int(fraction[1])
 
     if Y == 0:
-        raise ValueError("Denominator cannot be zero")
+        raise ZeroDivisionError("Denominator cannot be zero")
     if X > Y:
         raise ValueError("Numerator cannot be greater than denominator")
 
-    percentage = (X/Y) * 100
+    percentage = int((X/Y) * 100)
 
     return percentage
 
 
 def gauge(percentage):
-    converted_percentage = convert(percentage)
-
-    if converted_percentage<= 1:
+    if percentage <= 1:
         return "E"
-    elif converted_percentage >= 99:
+    elif percentage >= 99:
         return "F"
     else:
-        return f"{converted_percentage:.0f}%"
+        return f"{percentage:.0f}%"
 
 
 if __name__ == "__main__":
